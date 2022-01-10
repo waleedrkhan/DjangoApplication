d647548aa1e2a0c22891a26b6fd144328cbe9ecb
waleed2590 <waleedrkhan90@gmail.com>
2022-01-10 11:50:30 +0500

    added tokens

--- MyApplication/urls.py
+++ MyApplication/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyApplication.myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+path('hello/', views.HelloView.as_view()),
 ]
